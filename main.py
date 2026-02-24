716fcac9098b71f18f8506dd6141a58c0e58a461
fredc19 <f.cuthbertson2@newcastle.ac.uk>
2026-02-24 01:01:52 +0000

    INitialised DB

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key
 
+
+#CONFIG SQLALCH
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+#DB model
+
+
+
+
+
+
+#routes
 @app.route("/")
-def index():
-    return "Hello World!"
+def home():
+    if "username"in session:
+        return redirect(url_for('dashboard'))
+    return render_template("index.html")
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

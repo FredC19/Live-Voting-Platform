8f2d69edff7663c2f0f0b3d41f0c59bfdb692b10
fredc19 <f.cuthbertson2@newcastle.ac.uk>
2026-02-26 02:27:42 +0000

    reveryed change

--- main.py
+++ main.py
@@ -215,11 +215,7 @@ def load_polls():
 
     return render_template("poll_chunk.html", polls=polls)
 
-@app.route("/reset-db-xk92")
-def reset_db():
-    db.drop_all()
-    db.create_all()
-    return "Done"
+
 
 
 
